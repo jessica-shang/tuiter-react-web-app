dbb6f3db1ff29c8884b62cf0efdcc842ce2ed03d
Jessica Shang <jesss.yujie@gmail.com>
2023-03-26 13:37:31 -0400

    explore navigation fixed

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 const NavigationSidebar = (
     {
@@ -6,44 +7,41 @@ const NavigationSidebar = (
     }
 ) => {
   return (
-      <div className="list-group">
-        <a className="list-group-item">Tuiter</a>
-        <a className={`list-group-item
-                    ${active === 'home'?'active':''}`}>
-          Home
-        </a>
-        <a className={`list-group-item
-                    ${active === 'explore'?'active':''}`}>
-          Explore
-        </a>
-        <a className={`list-group-item
-                    ${active === 'notifications'?'active':''}`}>
-          Notifications
-        </a>
-        <a className={`list-group-item
-                    ${active === 'messages'?'active':''}`}>
-          Messages
-        </a>
-        <a className={`list-group-item
-                    ${active === 'bookmarks'?'active':''}`}>
-          Bookmarks
-        </a>
-        <a className={`list-group-item
-                    ${active === 'lists'?'active':''}`}>
-          Lists
-        </a>
-        <a className={`list-group-item
-                    ${active === 'profile'?'active':''}`}>
-          Profile
-        </a>
-        <a className={`list-group-item
-                    ${active === 'more'?'active':''}`}>
-          More
-        </a>
+      <div className="list-group mb-1">
+        <a className={"list-group-item"}><i
+            className="fa-brands fa-twitter"/><span
+            className="d-none d-xl-inline"> Tuiter</span></a>
+        <Link to="/tuiter/home" className={`list-group-item ${active === 'home' ? 'active' : ''}`}><i
+            className="fa-solid fa-house-chimney"/><span
+            className="d-none d-xl-inline"> Home</span></Link>
+        <Link to="/tuiter/explore" className={`list-group-item ${active === 'explore' ? 'active'
+                                                                                      : ''}`}><i
+            className="fa-solid fa-hashtag"/><span
+            className="d-none d-xl-inline"> Explore</span></Link>
+        <a className={`list-group-item ${active === 'notifications' ? 'active'
+                                                                    : ''}`}> <i
+            className="fa-solid fa-bell"/><span
+            className="d-none d-xl-inline"> Notifications</span></a>
+        <a className={`list-group-item ${active === 'messages' ? 'active'
+                                                               : ''}`}><i
+            className="fa-solid fa-envelope"/><span
+            className="d-none d-xl-inline"> Messages</span></a>
+        <a className={`list-group-item ${active === 'bookmarks' ? 'active'
+                                                                : ''}`}><i
+            className="fa-solid fa-bookmark"/><span
+            className="d-none d-xl-inline"> Bookmarks</span></a>
+        <a className={`list-group-item ${active === 'lists' ? 'active'
+                                                            : ''}`}><i
+            className="fa-solid fa-list"/><span
+            className="d-none d-xl-inline"> List</span></a>
+        <a className={`list-group-item ${active === 'profile' ? 'active'
+                                                              : ''}`}><i
+            className="fa-solid fa-user"/><span
+            className="d-none d-xl-inline"> Profile</span></a>
+        <a className={`list-group-item ${active === 'more' ? 'active' : ''}`}><i
+            className="fa-solid fa-ellipsis"/><span
+            className="d-none d-xl-inline"> More</span></a>
       </div>
   );
 };
 export default NavigationSidebar;
-
-
-
