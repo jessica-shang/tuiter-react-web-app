5742ea114fc03d8fed15edf67ef7e03d96d8a8de
Jessica Shang <jesss.yujie@gmail.com>
2023-04-02 16:31:30 -0400

    a8 updateLikesHandler test

--- src/tuiter/tuits/tuit-stats.js
+++ src/tuiter/tuits/tuit-stats.js
@@ -1,12 +1,26 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { updateTuitLikes } from "./tuits-reducer";
+// import { updateTuitLikes } from "./tuits-reducer";
 import {updateTuitThunk} from "../../services/tuits-thunks";
 
 const TuitStats = ({tuit}) => {
   const dispatch = useDispatch();
-  const updateTuitLikesHandler = (id) => {
-    dispatch(updateTuitLikes(id));
+  const updateLikesHandler = () => {
+    const updatedTuit = {
+      ...tuit,
+      likes : tuit.liked ? tuit.likes - 1 : tuit.likes + 1,
+      liked : !tuit.liked
+    };
+    dispatch(updateTuitThunk(updatedTuit))
+  }
+
+  const updateDislikesHandler = () => {
+    const updatedTuit = {
+      ...tuit,
+      dislikes : tuit.disliked ? tuit.dislikes - 1 : tuit.dislikes + 1,
+      disliked : !tuit.disliked
+    };
+    dispatch(updateTuitThunk(updatedTuit))
   }
 
   return(
@@ -22,20 +36,20 @@ const TuitStats = ({tuit}) => {
           <span className="m-2">{tuit.retuits}</span>
         </div>
 
-        <div className="col">
-          <i onClick={() => dispatch(updateTuitThunk({
-                                                       ...tuit,
-                                                       likes: tuit.likes + 1
-                                                     }))} className="bi bi-heart-fill me-2 text-danger"/>
-          <span> {tuit.likes}</span>
+        <div>
+         <span onClick={updateLikesHandler}>
+            {tuit.liked && <i className="bi bi-heart-fill text-danger"></i>}
+           {!tuit.liked && <i className="bi bi-heart"></i>}
+           <span className="m-2">{tuit.likes}</span>
+          </span>
         </div>
 
-        <div className="col">
-          <i onClick={() => dispatch(updateTuitThunk({
-                                                       ...tuit,
-                                                       dislikes: tuit.dislikes + 1
-                                                     }))} className="bi bi-hand-thumbs-down-fill me-2"/>
-          <span> {tuit.dislikes}</span>
+        <div>
+          <span onClick={updateDislikesHandler}>
+            {tuit.disliked && <i className="bi bi-hand-thumbs-down-fill"></i>}
+            {!tuit.disliked && <i className="bi bi-hand-thumbs-down"></i>}
+            <span className="m-2">{tuit.likes}</span>
+          </span>
         </div>
 
         <div>
